d869b565d7f191b5e85f1e4d8946725219f7feba
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2025-08-06 16:31:48 +0300

    ssr false

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,16 @@
-import ProductsClient from './ProductsClient';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
+const ProductsClient = dynamic(() => import('./ProductsClient'), {
+  ssr: false,
+});
 
 const Products = () => {
-  return <ProductsClient />;
+  return (
+    <Suspense fallback={<div>Завантаження...</div>}>
+      <ProductsClient />
+    </Suspense>
+  );
 };
 
 export default Products;
